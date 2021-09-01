57bdc0db1deb6171f1cedab1758b5053721ea284
Jacob Kapp <60153783+jkapp610@users.noreply.github.com>
2021-08-31 21:52:53 -0400

    Initial upload of yelp camp

---
+++ YelpCamp/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get("/", function(req,res){
+
+    res.render("home");
+
+})
+
+app.listen(3000,function() {
+    console.log(" APP is listining ON PORT 3000!")
+})
